d6d17584b4c25b0783a7d1a944709d3396630432
Daniel Ribeiro <danielribeirosoaressilva@gmail.com>
2019-02-24 03:02:25 -0300

    Fixing the logic

--- timer.js
+++ timer.js
@@ -2,6 +2,7 @@ var countDownDate = new Date("Mar 9, 2019 00:00:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
+    //var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -10,17 +11,13 @@ var x = setInterval(function() {
 
     if(days>0){
         element.innerHTML = "<table> <tr><th>"+days+"</th> <td>Days</td></tr> <tr><th>"+hours+"</th> <td>Hours</td></tr> <tr><th>"+minutes+"</th> <td>Minutes</td></tr><tr><th>"+seconds+"</th><td>Seconds</td> </tr> </table>";
-    }
-    if(days == 0){
+    }else if(hours > 0){
         element.innerHTML = "<table> <tr><th>"+hours+"</th> <td>Hours</td></tr> <tr><th>"+minutes+"</th> <td>Minutes</td></tr><tr><th>"+seconds+"</th><td>Seconds</td> </tr> </table>";
-    }
-    if(hours == 0){
+    }else if(minutes > 0){
         element.innerHTML = "<table> <tr><th>"+minutes+"</th> <td>Minutes</td></tr><tr><th>"+seconds+"</th><td>Seconds</td> </tr> </table>";
-    }
-    if(minutes == 0){
+    }else if(seconds > 0){
         element.innerHTML = "<table> </tr><tr><th>"+seconds+"</th><td>Seconds</td> </tr> </table>";
-    }
-    if (distance < 0) {
+    }else if (distance < 0) {
         clearInterval(x);
         element.innerHTML = "<h1 class='soon'>Soon</h1>";
     }
